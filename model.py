02377506f313a9691c08412ef36c0255bdac5d37
Peter V. <128387629+VrabelPeter@users.noreply.github.com>
2025-03-17 19:59:27 +0100

    Update model.py
    Remove comments

--- model.py
+++ model.py
@@ -14,17 +14,14 @@ class DQN(nn.Module):
             `input_shape`: The shape of the input tensor (C, H, W).
             `n_actions`: The number of actions the agent can take.
         """
-        # The input to the network is Bx4x84x84 in the original paper
         self.conv = nn.Sequential(
-            # Creates tensor Bx32x20x20 of feature maps when applied
             nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),  # Bx64x9x9
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),  # Bx64x7x7
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU(),
-            # To avoid computing the size by hand
-            nn.Flatten(),  # B times 3,136
+            nn.Flatten(),
         )
         size = self.conv(torch.zeros(1, *input_shape)).size()[-1]
         self.fc = nn.Sequential(
